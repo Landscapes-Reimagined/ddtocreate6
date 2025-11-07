ee0987785b45327d06689e46d00160da1759d02d
Gamma_02 <gammagames06@gmail.com>
2025-11-06 19:58:36 -0800

    update to create 6.0.8!

--- src/main/java/com/landscapesreimagined/ddtocreate6/util/Redirects.java
+++ src/main/java/com/landscapesreimagined/ddtocreate6/util/Redirects.java
@@ -3,7 +3,12 @@ package com.landscapesreimagined.ddtocreate6.util;
 import com.landscapesreimagined.ddtocreate6.replaced.DDStress;
 import com.landscapesreimagined.ddtocreate6.util.mixin.StressValueProvider;
 import com.landscapesreimagined.ddtocreate6.util.mixin.TurretAccessor;
+import com.simibubi.create.AllTags;
 import com.simibubi.create.api.stress.BlockStressValues;
+import com.tterrag.registrate.builders.BlockBuilder;
+import com.tterrag.registrate.builders.ItemBuilder;
+import com.tterrag.registrate.util.nullness.NonNullFunction;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.ModLoadingContext;
 import uwu.lopyluna.create_dd.block.BlockProperties.potato_turret.PotatoTurretBlockEntity;
@@ -22,6 +27,20 @@ public class Redirects {
 //        BlockStressValues.RPM.registerProvider(stress::getGeneratedRPM);
 
     }
+
+    @SuppressWarnings({"UnstableApiUsage", "removal"})
+    public static <T extends Block, P> NonNullFunction<BlockBuilder<T, P>, ItemBuilder<BlockItem, BlockBuilder<T, P>>> tagBlockAndItem(
+            String... path) {
+        return b -> {
+            for (String p : path)
+                b.tag(AllTags.forgeBlockTag(p));
+            ItemBuilder<BlockItem, BlockBuilder<T, P>> item = b.item();
+            for (String p : path)
+                item.tag(AllTags.forgeItemTag(p));
+            return item;
+        };
+    }
+
 
     public static float getTurretY(PotatoTurretBlockEntity be){
         return 0.0f;
